def9d1f963d3bfb989d80f6120f454e3ff286854
binyot <sxterminal@protonmail.com>
2019-02-17 23:44:20 +0300

    add vector3

--- test.py
+++ test.py
@@ -1,10 +1,9 @@
-from ONVIFCameraControl1 import ONVIFCameraControl
-from time import sleep
+from ONVIFCameraControl import ONVIFCameraControl as OCC
+from vector3 import vector3
 
-
-cams = [ONVIFCameraControl('192.168.15.43', 80, 'admin', 'Supervisor'),]
-
-for c in cams:
-	print('testing movement')
-
-	c.move_up()
+cam = OCC('192.168.15.43', 80, 'admin', 'Supervisor', '/home/akarin/PycharmProjects/vivif/wsdl')
+#cam.absoluteMove(-1, -1, 0)
+cam.goHome()
+cam.continuousMove(vector3(-0.1, -0.1, -0.1), 3)
+cam.continuousMove(vector3(0, +0.2, +0.2), 3)
+cam.goHome()
---
+++ vector3.py
@@ -0,0 +1,36 @@
+import math
+
+
+class vector3:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __str__(self):
+        return "(" + str(self.x) + ", " + str(self.y) + ", " + str(self.z) + ")"
+
+    def get_length(self):
+        return math.sqrt(self.x ** 2 + self.y ** 2 + self.z ** 2)
+
+    def __add__(self, v):
+        return vector3(self.x + v.x, self.y + v.y, self.z + v.z)
+
+    def __sub__(self, v):
+        return vector3(self.x - v.x, self.y - v.y, self.z - v.z)
+
+    def __mul__(self, n):
+        return vector3(self.x * n, self.y * n, self.z * n)
+
+    def normalize(self, inplace=False):
+        if inplace:
+            for v in [self.x, self.y, self.z]:
+                v = v/self.get_length()
+        else:
+            return vector3(self.x/self.get_length(),
+                           self.y/self.get_length(),
+                           self.z/self.get_length())
+
+    @staticmethod
+    def dot_product(v1, v2):
+        return (v1.x * v2.x + v1.y * v2.y + v1.z * v2.z)
